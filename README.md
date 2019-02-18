7004221009cda7175a8e517c03b4a5911cdf4bd4
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-02-19 01:25:18 +0530

    ADD arg parser

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # keras-lstm
 This is a sample LSTM implementation in Keras
+
+#### Setup
+```
+pip install -r requirements.txt
+mkdir -p logs models
+```
+
+#### Train 
+```
+python train.py --lr 1e-3 --decay 1e-5 --epochs 100 --shuffle True
+```
+	
--- train.py
+++ train.py
@@ -1,9 +1,25 @@
 import time
+import argparse
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.callbacks import TensorBoard
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--lr", type=float, help="the learning rate", default=1e-3)
+parser.add_argument("--decay", type=float, help="the learning rate decay", default=1e-5)
+parser.add_argument("--epochs", type=int, help="the number of epochs", default=100)
+parser.add_argument("--shuffle", type=bool, help="shuffle between train and validation data while training", default=True)
+
+args = parser.parse_args()
+
+#hyperparameters
+_learning_rate=args.lr
+_decay_rate=args.decay
+_epochs=args.epochs
+_shuffle=args.shuffle
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
@@ -27,7 +43,7 @@ model.add(Dropout(0.2))
 
 model.add(Dense(10, activation='softmax'))
 
-opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+opt = tf.keras.optimizers.Adam(lr=_learning_rate, decay=_decay_rate)
 
 model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
@@ -36,6 +52,6 @@ model.summary()
 tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
                           write_graph=True, write_images=False)
 
-model.fit(x_train, y_train, epochs=3, validation_data=(x_test,y_test), shuffle=True, callbacks=[tensorboard])
+model.fit(x_train, y_train, epochs=_epochs, validation_data=(x_test,y_test), shuffle=_shuffle, callbacks=[tensorboard])
 
-model.save('models/model'+ str(time.ctime(int(time.time()))).replace(' ','_') +'.h5')
+model.save('models/model'+ str(time.ctime(int(time.time()))).replace(' ','_') + '_' + str(_epochs) +'.h5')
